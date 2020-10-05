a0e0f793b41d7a4eaa1c9da86b9a75eb6be2f750
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2020-10-05 15:54:04 -0300

    Fix measurement anchor being placed through UI clicks

--- Assets/InteriorDesignSim/Scripts/Gameplay/MeasureAnchorPlacementInteractable.cs
+++ Assets/InteriorDesignSim/Scripts/Gameplay/MeasureAnchorPlacementInteractable.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.Interaction.Toolkit.AR;
 
 namespace XRAccelerator.Gameplay
@@ -8,9 +9,13 @@ namespace XRAccelerator.Gameplay
         [NonSerialized]
         public bool IsPlacementEnabled;
 
+        // [XRToolkitWorkaround] ARPlacementInteractable doesn't allow disabling / enabling placement
+        // [XRToolkitWorkaround] Gestures disregard if the touch was handled by the EventSystem (UI Canvas)
         protected override bool CanStartManipulationForGesture(TapGesture gesture)
         {
-            return IsPlacementEnabled && base.CanStartManipulationForGesture(gesture);
+            return IsPlacementEnabled &&
+                !EventSystem.current.IsPointerOverGameObject(gesture.FingerId) &&
+                base.CanStartManipulationForGesture(gesture);
         }
     }
 }
