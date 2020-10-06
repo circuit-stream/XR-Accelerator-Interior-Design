aeaaab16ce3e2ae78e8ecf4dffc9bada330c181b
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2020-10-06 17:26:46 -0300

    Fix furniturePreview rotation logic

--- Assets/InteriorDesignSim/Scripts/Gameplay/SafeARInteractions/SafeARRotationInteractable.cs
+++ Assets/InteriorDesignSim/Scripts/Gameplay/SafeARInteractions/SafeARRotationInteractable.cs
@@ -1,10 +1,15 @@
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.AR;
 
 namespace XRAccelerator.Gameplay
 {
     public class SafeARRotationInteractable : ARRotationInteractable
     {
+        [SerializeField]
+        [Tooltip("If this object should always try to be selected")]
+        private bool forceSelection;
+
         private bool wasDestroyed;
 
         // [XRToolkitWorkaround] Something is keeping a reference to this interactable
@@ -31,6 +36,13 @@ namespace XRAccelerator.Gameplay
         protected override bool CanStartManipulationForGesture(TwistGesture gesture)
         {
             return !wasDestroyed && base.CanStartManipulationForGesture(gesture);
+        }
+
+        protected override bool IsGameObjectSelected()
+        {
+            // [XRToolkitWorkaround] Force selection is used here so the preview can rotate without being tapped before
+            // XRToolkit provides no way to select an interactable by code.
+            return !wasDestroyed && (forceSelection || base.IsGameObjectSelected());
         }
     }
 }
